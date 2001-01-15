b2a04f081ef7f5d7226490767d191eb88270fc8d
sms <>
2001-01-15 20:41:26 +0000

    + poll: Added support for deleting dial-counters (Binkley-, ifcico- and  BinkD-style)

--- tools/poll.c
+++ tools/poll.c
@@ -2,8 +2,10 @@
 
 #include <errno.h>
 #include <fidoconf/fidoconf.h>
+#include <smapi/progprot.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "general.h"
 
 int main(int argc, char *argv[])
@@ -12,8 +14,9 @@ int main(int argc, char *argv[])
 
   if (argc < 2)
   {
-    printf("poll - poll (create a flowfile for) a node/point\n");
-    printf("Syntax: poll <address> [<flavour>]\n");
+    printf("poll - poll (create a flowfile for) a node/point\n"
+           "Syntax: poll <address> [deldlc] [<flavour>]\n"
+	   "        where <flavour> is one of: immediate, crash, normal, direct, hold\n");
 
     return 1;
   }
@@ -23,23 +26,32 @@ int main(int argc, char *argv[])
   if (config != NULL)
   {
     char *flo = FLOName(config, Str2Addr(argv[1]));
+    unsigned int floLen = strlen(flo);
     FILE *f;
+    int deldlc = 0;
+    int i;
+
+    // default flavour
+    flo[floLen - 3] = 'c';
 
     if (argc > 2)
     {
-      if (stricmp(argv[2], "immediate") == 0) flo[strlen(flo)-3] = 'i';
-      else if (stricmp(argv[2], "crash") == 0) flo[strlen(flo)-3] = 'c';
-      else if (stricmp(argv[2], "normal") == 0) flo[strlen(flo)-3] = 'f';
-      else if (stricmp(argv[2], "direct") == 0) flo[strlen(flo)-3] = 'd';
-      else if (stricmp(argv[2], "hold") == 0) flo[strlen(flo)-3] = 'h';
-      else
+      for (i = 2; i < argc; i++)
       {
-	printf("Invalid flavour '%s'! Using crash.\n", argv[2]);
-	flo[strlen(flo)-3] = 'c';
+	if (stricmp(argv[i], "deldlc") == 0) deldlc = 1;
+	else if (stricmp(argv[i], "immediate") == 0) flo[strlen(flo)-3] = 'i';
+	else if (stricmp(argv[i], "crash") == 0) flo[strlen(flo)-3] = 'c';
+	else if (stricmp(argv[i], "normal") == 0) flo[strlen(flo)-3] = 'f';
+	else if (stricmp(argv[i], "direct") == 0) flo[strlen(flo)-3] = 'd';
+	else if (stricmp(argv[i], "hold") == 0) flo[strlen(flo)-3] = 'h';
+	else
+	{
+	  printf("Invalid option '%s'!\n", argv[i]);
+	}
       }
     }
-    else flo[strlen(flo)-3] = 'c';
 
+    // create FlowFile
     f = fopen(flo, "a");
     if (f == NULL)
     {
@@ -47,6 +59,25 @@ int main(int argc, char *argv[])
 	     strerror(errno));
     }
     else fclose(f);
+
+    if (deldlc)
+    {
+      // delete dial-counters
+      flo[floLen - 3] = '$'; // Binkley-style
+      flo[floLen - 2] = '$';
+      flo[floLen - 1] = '0';
+      if (fexist(flo)) unlink(flo);
+
+      flo[floLen - 3] = 's'; // ifcico-style
+      flo[floLen - 2] = 't';
+      flo[floLen - 1] = 's';
+      if (fexist(flo)) unlink(flo);
+
+      flo[floLen - 3] = 'h'; // BinkD-style
+      flo[floLen - 2] = 'l';
+      flo[floLen - 1] = 'd';
+      if (fexist(flo)) unlink(flo);
+    }
 
     free(flo);
   }
